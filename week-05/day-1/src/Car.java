88a2a0f48e730ccfd66f8a31b5ba9a29d2fc5558
regnisalram <reka.bence@gmail.com>
2016-11-14 13:51:23 +0100

    initial commit of week-05 material, workshop01

---
+++ week-05/day-1/src/Car.java
@@ -0,0 +1,26 @@
+/**
+ * Created by regnisalram on 11/14/16.
+ */
+// Car.java
+public class Car {
+    //Add somethings here to create a car and some fields
+    int sizeEngine;
+    String color;
+
+    Car() {
+        this.sizeEngine = 1600;
+        this.color = "red";
+    }
+
+    int getSizeEngine() {
+        return sizeEngine;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
