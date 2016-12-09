f760333d344e15aa3b408d8db231f88a75f9e458
regnisalram <reka.bence@gmail.com>
2016-12-09 14:10:38 +0100

    cleaned up Tile, Floor, Wall, deleted unnecessary params

--- week-07/project/src/Tile.java
+++ week-07/project/src/Tile.java
@@ -7,16 +7,8 @@ import java.awt.*;
 abstract public class Tile extends GameObject{
 
     protected boolean isWalkable;
-    protected Image surface;
-    int posX, posY;
 
     public Tile(String filename, int posX, int posY) {
         super(filename, posX, posY);
     }
-
-
-    public Image getSurface() {
-        return surface;
-    }
-
 }
--- week-07/project/src/Wall.java
+++ week-07/project/src/Wall.java
@@ -7,5 +7,6 @@ public class Wall extends Tile {
 
     public Wall(int posX, int posY) {
         super("images/wall.png", posX, posY);
+        isWalkable = false;
     }
 }
