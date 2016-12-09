fea40d720466f775c33912d24cbd3085617a375e
regnisalram <reka.bence@gmail.com>
2016-12-09 14:13:07 +0100

    Character moveCharacter method added (can't move out of the map or into walls)

--- week-07/project/src/Character.java
+++ week-07/project/src/Character.java
@@ -7,17 +7,39 @@ import java.io.IOException;
  */
 abstract public class Character extends GameObject{
 
+    int updatedPosX;
+    int updatedPosY;
+
     public Character(String filename, int posX, int posY) {
         super(filename, posX, posY);
     }
 
-    public void moveCharacter(String filename, int moveX, int moveY) {
-        setPosX(getPosX() + moveX);
-        setPosY(getPosY() + moveY);
+    public void moveCharacter(String filename, int moveX, int moveY, Map map) {
+        updatedPosX = getPosX() + moveX;
+        updatedPosY = getPosY() + moveY;
+        if (isWalkable(map)) {
+            if (!(updatedPosX < 0 || updatedPosX > 9)) {
+                    setPosX(updatedPosX);
+            }
+            if (!(updatedPosY < 0 || updatedPosY > 10)) {
+                    setPosY(updatedPosY);
+            }
+        }
+
         try{
             image = ImageIO.read(new File(filename));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    public boolean isWalkable(Map map) {
+        try {
+            return map.originalBoard[updatedPosY][updatedPosX] != 0;
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        return false;
+    }
+
 }
