6db02745852bca46c9bee2bb97d1845ac2da1b48
regnisalram <reka.bence@gmail.com>
2016-12-14 11:00:39 +0100

    removed practice week folder & deleted files

--- practice_week/friday/src/App.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by regnisalram on 12/2/16.
- */
-public class App {
-}
--- practice_week/thursday/src/Animal.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by regnisalram on 12/1/16.
- */
-public interface Animal {
-
-    void move(int distance);
-
-    void feed();
-}
