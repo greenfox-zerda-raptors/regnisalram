f130697243594c87882a644575e5c617fb86f889
regnisalram <reka.bence@gmail.com>
2016-12-14 13:52:48 +0100

    account toString method added, Workshop02 added

--- week-08/day-3/src/Account.java
+++ week-08/day-3/src/Account.java
@@ -36,4 +36,9 @@ public class Account {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
