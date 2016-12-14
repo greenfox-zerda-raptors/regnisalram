6cc87ba167cb3aeac3698d07688dcf9f1e82da86
regnisalram <reka.bence@gmail.com>
2016-12-14 14:11:53 +0100

    additional accounts added

--- week-08/day-3/src/Workshop02.java
+++ week-08/day-3/src/Workshop02.java
@@ -7,6 +7,8 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Workshop02 {
     public static void main(String[] args) throws SQLException {
@@ -21,8 +23,21 @@ public class Workshop02 {
         Account account = new Account("Captain America", "uejnsd632**234.");
         createAccountIfNotExists(accountDao, account);
 
-        account = accountDao.queryForId("Captain America");
-        System.out.println("Account: " + account.toString());
+        ArrayList<String> accountNames = new ArrayList<>();
+        accountNames.add("Iron Man");
+        accountNames.add("Wolverine");
+        accountNames.add("Hulk");
+        accountNames.add("Loki");
+
+        for (String name : accountNames) {
+            createAccountIfNotExists(accountDao, new Account(name, "abc"));
+        }
+
+        List<Account> accounts = accountDao.queryForAll();
+
+        for (Account acc : accounts) {
+            System.out.println("Account: " + acc.toString());
+        }
     }
 
     private static void createAccountIfNotExists(Dao<Account, String> accountDao, Account acc) throws SQLException {
