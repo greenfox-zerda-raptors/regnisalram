6cd7494478f02dd866f729beaad553502595f047
regnisalram <reka.bence@gmail.com>
2016-12-19 14:10:23 +0100

    hello world web app added

---
+++ week-09/day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by regnisalram on 12/19/16.
+ */
+@Controller
+public class HelloWebController {
+
+    @RequestMapping("/web/greeting")
+    public String greeting(Model model) {
+        model.addAttribute("name", " World");
+        return "greeting";
+    }
+}
