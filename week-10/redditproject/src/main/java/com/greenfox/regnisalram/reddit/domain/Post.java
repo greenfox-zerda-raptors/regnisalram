fe275a2f3c6013b0dbff8f150d49a1fce368c5f1
regnisalram <reka.bence@gmail.com>
2017-01-04 14:08:43 +0100

    Post class added

--- week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/domain/Post.java
+++
@@ -1,7 +0,0 @@
-package com.greenfox.regnisalram.reddit.domain;
-
-/**
- * Created by regnisalram on 1/4/17.
- */
-public class Post {
-}
---
+++ week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/models/Post.java
@@ -0,0 +1,49 @@
+package com.greenfox.regnisalram.reddit.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by regnisalram on 1/4/17.
+ */
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String postTitle;
+    private String postMessage;
+    private int score;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostMessage() {
+        return postMessage;
+    }
+
+    public void setPostMessage(String postMessage) {
+        this.postMessage = postMessage;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
