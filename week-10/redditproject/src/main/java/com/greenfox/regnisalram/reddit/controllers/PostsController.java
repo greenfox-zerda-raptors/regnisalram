186e98aeee69d3cc057f93e0e2ca500164fdc410
regnisalram <reka.bence@gmail.com>
2017-01-04 15:56:52 +0100

    new post method added (form posts)

--- week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/controllers/PostsController.java
+++ week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/controllers/PostsController.java
@@ -1,11 +1,14 @@
 package com.greenfox.regnisalram.reddit.controllers;
 
+import com.greenfox.regnisalram.reddit.models.Post;
 import com.greenfox.regnisalram.reddit.services.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by regnisalram on 1/4/17.
@@ -26,6 +29,12 @@ public class PostsController {
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String addPost(Model model) {
         return "posts/new_post";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ModelAndView create(@RequestParam("title") String title, @RequestParam("message") String message) {
+        repository.save(new Post(title, message));
+        return new ModelAndView("redirect:/posts/");
     }
 
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
