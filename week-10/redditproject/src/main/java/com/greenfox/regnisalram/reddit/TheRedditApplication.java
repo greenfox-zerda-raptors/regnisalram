f2726898b5a0dca22dbef69ce3e9bf5e9256583b
regnisalram <reka.bence@gmail.com>
2017-01-06 14:00:53 +0100

    db now updates, favicon added :)

--- week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/TheRedditApplication.java
+++ week-10/redditproject/src/main/java/com/greenfox/regnisalram/reddit/TheRedditApplication.java
@@ -1,26 +1,12 @@
 package com.greenfox.regnisalram.reddit;
 
-import com.greenfox.regnisalram.reddit.models.Post;
-import com.greenfox.regnisalram.reddit.services.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheRedditApplication implements CommandLineRunner {
-
-	@Autowired
-	private PostRepository repository;
-
+public class TheRedditApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheRedditApplication.class, args);
-	}
-
-	public void run(String... strings) throws Exception {
-		for (int i = 1; i < 6; i++) {
-			repository.save(new Post("Title of post", "This is a post " + i));
-		}
 	}
 }
